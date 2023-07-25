edce359b94e3133e7082c6774bcdb7d8fd3a7d51
Guruprasanth302 <pguru1218@gmail.com>
2023-07-25 14:57:42 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const { db } = require('./db/db')
+const {readdirSync} = require('fs');
+const { route } = require('./routes/transactions');
+
+require('dotenv').config()
+
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+const server = () => {
+  db()
+  app.listen(PORT, () => {
+    console.log('Listening to port ', PORT)
+  })
+}
+
+server()
